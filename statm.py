fe87c54d193d08045c13df5719c74eaca44cb975
EC2 Default User <ec2-user@ip-172-31-13-201.ap-northeast-1.compute.internal>
2023-11-22 14:42:23 +0000

    update

--- statm.py
+++ statm.py
@@ -9,80 +9,48 @@ from datetime import datetime, timedelta
 import os
 import json
 
-
-def split_big_range(begin_prefix: str, end_prefix: str)->list:
-    datelength=10
-    result = list()
-    result.append(begin_prefix)
-    begin_dt_index = begin_prefix.rfind('_')
-    if begin_dt_index < 0:
-        return [begin_prefix, end_prefix]
-    begin_dt_str = begin_prefix[begin_dt_index+1:begin_dt_index+datelength+1]
-
-    end_dt_index = end_prefix.rfind('_')
-    end_dt_str = end_prefix[end_dt_index+1:end_dt_index+datelength+1]
-
-    format_string = "%Y%m%d"
-    format2_string = "%Y%m%d%H"
-    
-    begin_date = datetime.strptime(begin_dt_str, format_string)
-    end_date = datetime.strptime(end_dt_str, format_string)
-
-    current_date = begin_date + timedelta(hours=1)
-    begin_f = begin_prefix[:begin_dt_index+1]
-    while current_date < end_date:
-        current_str = current_date.strftime(format2_string)
-        new_sp = begin_f + current_str
-        result.append(new_sp)
-        current_date = current_date+ timedelta(hours=1)
-    result.append(end_prefix)
-
-    return result
-    
-    
+bach_count = 10000
 
 
-def exe_check(host_source: str, table_name: str, b_prefix: str, e_prefix: str, cache: dict, max_count=1000000):
-    
-    
-    hour = 1000*60*60 
+def exe_check(host_source: str, table_name: str, begin_prefix: str, end_prefix: str, cache: dict, max_count=1000000):
+    hour = 1000 * 60 * 60
     host = host_source.split(':')
 
-    range_fix = split_big_range(b_prefix, e_prefix)
-    for index_fix in range(0, len(range_fix)-1):
+    # 分批的原因是服务端rpc只能连接1分钟，但是服务的目前还不能改
+    connection = happybase.Connection(host[0], port=int(host[1]), timeout=hour)
+    connection.open()
+    table = connection.table(table_name)
 
-        begin_prefix = range_fix[index_fix]
-        end_prefix = range_fix[index_fix+1]
-        # 分批的原因是服务端rpc只能连接1分钟，但是服务的目前还不能改
-        connection = happybase.Connection(host[0], port=int(host[1]), timeout=hour)
-        connection.open()
-        table = connection.table(table_name)
+    # 构造扫描器，并应用过滤器
 
-
-        # scan_filter = f"RowFilter(>, 'binaryprefix:{begin_prefix}') AND RowFilter(<, 'binaryprefix:{end_prefix}')"
-        # 构造扫描器，并应用过滤器
-        try:
-            print(f"{host_source} bescagin scan table {table_name} in {begin_prefix} and {end_prefix}")
-            ca = dict()
-            scanner = table.scan(row_start=begin_prefix,row_stop=end_prefix,sorted_columns=True)
-            counter = 0
+    try:
+        total_count = 0
+        row_start = begin_prefix
+        while True:
+            print(f"{host_source} begin scan scan table {table_name} in {row_start} and {end_prefix}")
+            scanner = table.scan(row_start=row_start, row_stop=end_prefix, sorted_columns=True, limit=bach_count)
+            one_count = 0
             for key, data in scanner:
-                ca[key] = data
-                counter += 1
-                if counter % 10000 == 0:
-                    print(f"正在扫描 {counter} 行")
+                cache[key] = data
+                one_count += 1
+                row_start = key
 
-        except Exception as e:
-            ca.clear()
-            print(e)
-            print(f'{begin_prefix} and {end_prefix} contain too much rows(> {max_count})')
+            total_count = len(cache)
+            print(f"正在扫描 {total_count} 行,新增 {one_count}")
+            # 包括上次的row key
+            if one_count <= 1:
+                break
+            if total_count > max_count:
+                break
 
-        cache.update(ca)
-        del ca
-        connection.close()
+    except Exception as e:
+        print(e)
 
-def compare(task_fix,host_source, host_target, table_name:str, result1: dict, result2: dict, begin_prefix, end_prefix):
+    connection.close()
 
+
+def compare(task_fix, host_source, host_target, table_name: str, result1: dict, result2: dict, begin_prefix,
+            end_prefix):
     l1 = len(result1)
     l2 = len(result2)
 
@@ -104,14 +72,14 @@ def compare(task_fix,host_source, host_target, table_name:str, result1: dict, re
 
         if len(notin_target_keys) > 0:
             is_true = False
-            file.write(f'如下key 在{host_source} 但是不在 {host_target}:\n')
+            file.write(f'如下key 共计 {len(notin_target_keys)}条在{host_source} 但是不在 {host_target}:\n')
             for key in notin_target_keys:
                 file.write(f'{key}\n')
             file.write('\n')
 
         if len(notin_source_keys) > 0:
             is_true = False
-            file.write(f'如下key 在{host_target} 但是不在 {host_source}:\n')
+            file.write(f'如下key 共计 {len(notin_source_keys)}条 在{host_target} 但是不在 {host_source}:\n')
             for key in notin_source_keys:
                 file.write(f'{key}\n')
             file.write('\n')
@@ -121,26 +89,26 @@ def compare(task_fix,host_source, host_target, table_name:str, result1: dict, re
             for key in intersation:
                 if result1[key] != result2[key]:
                     notmatch_list.append(f'{key}:\n {result1[key]}\n{result2[key]}\n')
-            
+
             if len(notmatch_list) > 0:
-                file.write(f'如下key 在两个集群都存在， 但是不一致,共计{len(notmatch_list)}:\n')
+                file.write(f'如下key 在两个集群都存在， 但是不一致,共计{len(notmatch_list)}条:\n')
                 for item in notmatch_list:
                     file.write(item)
                     file.write('\n')
         if is_true:
             file.write(f'校验结果：{table_name} 在两个集群在范围{begin_prefix} 和 {end_prefix} 数据验证全部正确\n')
 
-
         file.write('\t\n')
-
 
 
 def run(task_fix, host_source, host_target, table_name, begin_prefix, end_prefix, stat_count):
     # 创建两个线程
     cache1 = dict()
     cache2 = dict()
-    thread1 = threading.Thread(target=exe_check, args=(host_source, table_name, begin_prefix, end_prefix,cache1, stat_count,))
-    thread2 = threading.Thread(target=exe_check, args=(host_target, table_name, begin_prefix, end_prefix,cache2, stat_count,))
+    thread1 = threading.Thread(target=exe_check,
+                               args=(host_source, table_name, begin_prefix, end_prefix, cache1, stat_count,))
+    thread2 = threading.Thread(target=exe_check,
+                               args=(host_target, table_name, begin_prefix, end_prefix, cache2, stat_count,))
 
     # 启动线程
     print(f"begin scan table {table_name} between {host_source} and {host_target} in {begin_prefix} and {end_prefix}")
@@ -151,18 +119,21 @@ def run(task_fix, host_source, host_target, table_name, begin_prefix, end_prefix
     thread1.join()
     thread2.join()
 
-    print(f"begin compare table {table_name} between {host_source} and {host_target} in {begin_prefix} and {end_prefix}")
-    compare(task_fix,host_source, host_target, table_name, cache1,cache2, begin_prefix, end_prefix)
+    print(
+        f"begin compare table {table_name} between {host_source} and {host_target} in {begin_prefix} and {end_prefix}")
+    compare(task_fix, host_source, host_target, table_name, cache1, cache2, begin_prefix, end_prefix)
     print(f"end compare table {table_name} between {host_source} and {host_target} in {begin_prefix} and {end_prefix}")
 
     del cache1
     del cache2
 
-        
-        
 
 def main():
     arguments = sys.argv
+    if len(arguments) < 2:
+        print("缺少配置文件")
+        return
+
     conf_path = f"{os.getcwd()}/{arguments[1]}"
     print(conf_path)
     # 读取 JSON 文件
@@ -175,7 +146,7 @@ def main():
     max_count = conf['max_count']
     tables = conf['tables']
 
-    current_time = datetime.now()+timedelta(hours=8)
+    current_time = datetime.now() + timedelta(hours=8)
     # 将当前时间格式化为字符
     formatted_time = current_time.strftime("%Y-%m-%d_%H_%M_%S")
 
@@ -186,7 +157,7 @@ def main():
         end_prefix = table['end_prefix']
 
         run(formatted_time, host_source, host_target, table_name, begin_prefix, end_prefix, max_count)
-        
+
         end_time = time.time()
 
         # 计算程序运行时间
