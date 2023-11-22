2c39438db02f53549de5d184e805a13b396191e6
EC2 Default User <ec2-user@ip-172-31-13-201.ap-northeast-1.compute.internal>
2023-11-22 15:39:37 +0000

    update

--- statm.py
+++ statm.py
@@ -10,9 +10,21 @@ import os
 import json
 
 bach_count = 10000
+get_bach_count = 100
+
+def compare_get(result:dict, a:dict, b:dict):
+    t = list()
+    for row in b:
+        rkey = row[0]
+        data = row[1]
+        if a[rkey] != data:
+            result[rkey] = f'{rkey}不一致:\n{a[rkey]}\n{data}\n'
+        t.append(rkey)
+    return t
 
 
-def exe_check(host_source: str, table_name: str, begin_prefix: str, end_prefix: str, cache: dict, max_count=1000000):
+def exe_check(host_source: str, host_target:str, table_name: str, begin_prefix: str, end_prefix: str, result:dict,max_count=1000000):
+    start_time = time.time()
     hour = 1000 * 60 * 60
     host = host_source.split(':')
 
@@ -22,10 +34,11 @@ def exe_check(host_source: str, table_name: str, begin_prefix: str, end_prefix: 
     table = connection.table(table_name)
 
     # 构造扫描器，并应用过滤器
-
+    cache = dict()
     try:
         total_count = 0
         row_start = begin_prefix
+
         while True:
             print(f"{host_source} begin scan scan table {table_name} in {row_start} and {end_prefix}")
             scanner = table.scan(row_start=row_start, row_stop=end_prefix, sorted_columns=True, limit=bach_count)
@@ -48,84 +61,40 @@ def exe_check(host_source: str, table_name: str, begin_prefix: str, end_prefix: 
 
     connection.close()
 
+    host = host_target.split(':')
 
-def compare(task_fix, host_source, host_target, table_name: str, result1: dict, result2: dict, begin_prefix,
-            end_prefix):
-    l1 = len(result1)
-    l2 = len(result2)
+    # 分批的原因是服务端rpc只能连接1分钟，但是服务的目前还不能改
+    connection = happybase.Connection(host[0], port=int(host[1]), timeout=hour)
+    connection.open()
+    table = connection.table(table_name)
 
-    sourceset = set(result1.keys())
-    targetset = set(result2.keys())
+    request = list()
+    counter = 0
+    gen = cache.keys()
+    total_resp = list()
+    for key in gen:
+        request.append(key)
+        counter +=1
+        if counter == get_bach_count:
+            resp = table.rows(request)
+            t = compare_get(result, cache, resp)
+            total_resp.extend(t)
+            request = list()
 
-    notin_target_keys = sourceset.difference(targetset)
-    notin_source_keys = targetset.difference(sourceset)
-    intersation = sourceset.intersection(targetset)
+    if len(request) > 0:
+        resp = table.rows(request)
+        t = compare_get(result, cache, resp)
+        total_resp.extend(t)
 
-    with open(f'{os.getcwd()}/compare_{task_fix}.txt', 'a') as file:
-        file.write(f'{table_name} 在 {host_source} 在范围{begin_prefix} 和 {end_prefix} 扫描了: {l1}\n')
-        file.write(f'{table_name} 在 {host_target} 在范围{begin_prefix} 和 {end_prefix} 扫描了: {l2}\n')
+    diff = set(cache.keys()).difference(set(total_resp))
+    for key in diff:
+        result[key] = f'{key} 在集群{host_source} 但是不在{host_target}\n'
 
-        is_true = True
-        if l1 != l2:
-            file.write('校验结果：集群数据不一致\n')
-            is_true = False
+    end_time = time.time()
 
-        if len(notin_target_keys) > 0:
-            is_true = False
-            file.write(f'如下key 共计 {len(notin_target_keys)}条在{host_source} 但是不在 {host_target}:\n')
-            for key in notin_target_keys:
-                file.write(f'{key}\n')
-            file.write('\n')
-
-        if len(notin_source_keys) > 0:
-            is_true = False
-            file.write(f'如下key 共计 {len(notin_source_keys)}条 在{host_target} 但是不在 {host_source}:\n')
-            for key in notin_source_keys:
-                file.write(f'{key}\n')
-            file.write('\n')
-
-        if len(intersation) > 0:
-            notmatch_list = list()
-            for key in intersation:
-                if result1[key] != result2[key]:
-                    notmatch_list.append(f'{key}:\n {result1[key]}\n{result2[key]}\n')
-
-            if len(notmatch_list) > 0:
-                file.write(f'如下key 在两个集群都存在， 但是不一致,共计{len(notmatch_list)}条:\n')
-                for item in notmatch_list:
-                    file.write(item)
-                    file.write('\n')
-        if is_true:
-            file.write(f'校验结果：{table_name} 在两个集群在范围{begin_prefix} 和 {end_prefix} 数据验证全部正确\n')
-
-        file.write('\t\n')
-
-
-def run(task_fix, host_source, host_target, table_name, begin_prefix, end_prefix, stat_count):
-    # 创建两个线程
-    cache1 = dict()
-    cache2 = dict()
-    thread1 = threading.Thread(target=exe_check,
-                               args=(host_source, table_name, begin_prefix, end_prefix, cache1, stat_count,))
-    thread2 = threading.Thread(target=exe_check,
-                               args=(host_target, table_name, begin_prefix, end_prefix, cache2, stat_count,))
-
-    # 启动线程
-    print(f"begin scan table {table_name} between {host_source} and {host_target} in {begin_prefix} and {end_prefix}")
-    thread1.start()
-    thread2.start()
-
-    # 等待两个线程执行完毕
-    thread1.join()
-    thread2.join()
-
-    print(
-        f"begin compare table {table_name} between {host_source} and {host_target} in {begin_prefix} and {end_prefix}")
-    compare(task_fix, host_source, host_target, table_name, cache1, cache2, begin_prefix, end_prefix)
-    print(f"end compare table {table_name} between {host_source} and {host_target} in {begin_prefix} and {end_prefix}")
-
-    del cache1
-    del cache2
+        # 计算程序运行时间
+    elapsed_time = end_time - start_time
+    print(f"校验表{table_name} 耗时 {elapsed_time:.2f} 秒")
 
 
 def main():
@@ -150,19 +119,38 @@ def main():
     # 将当前时间格式化为字符
     formatted_time = current_time.strftime("%Y-%m-%d_%H_%M_%S")
 
+    result_list = list()
+    t = list()
     for table in tables:
-        start_time = time.time()
+        
         table_name = table['name']
         begin_prefix = table['begin_prefix']
         end_prefix = table['end_prefix']
+        result = dict()
+        th = threading.Thread(target=exe_check,
+                                 args=(host_source, host_target, table_name, begin_prefix, end_prefix,result, max_count,))
+        
+        result_list.append((table,result))
+        t.append(th)
+        th.start()
+    
+    for item in t:
+        item.join()
 
-        run(formatted_time, host_source, host_target, table_name, begin_prefix, end_prefix, max_count)
+    with open(f'{os.getcwd()}/compare_{formatted_time}.txt', 'a') as file:
+        for tup in result_list:
+            table = tup[0]
+            table_name = table['name']
+            begin_prefix = table['begin_prefix']
+            end_prefix = table['end_prefix']
+            result = tup[1]
 
-        end_time = time.time()
+            if len(result) > 0:
+                file.write(f'{table_name} 在 {host_source} 在范围{begin_prefix} 和 {end_prefix}\n')
+                for key in result:
+                    file.write(result[key])
+                file.write('\n')
 
-        # 计算程序运行时间
-        elapsed_time = end_time - start_time
-        print(f"校验表{table_name} 耗时 {elapsed_time:.2f} 秒")
 
 
 main()
