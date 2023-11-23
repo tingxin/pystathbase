33d35b141c54cba05facf832cfe148a99c40c313
EC2 Default User <ec2-user@ip-172-31-13-201.ap-northeast-1.compute.internal>
2023-11-23 01:58:12 +0000

    update

--- statr.py
+++ statr.py
@@ -2,7 +2,34 @@ import redis
 import sys
 import json
 import os
+import threading
 
+
+def run(task_fix, host_source, host_target, table_name, begin_prefix, end_prefix, stat_count):
+    # 创建两个线程
+    cache1 = dict()
+    cache2 = dict()
+    thread1 = threading.Thread(target=exe_check,
+                               args=(host_source, table_name, begin_prefix, end_prefix, cache1, stat_count,))
+    thread2 = threading.Thread(target=exe_check,
+                               args=(host_target, table_name, begin_prefix, end_prefix, cache2, stat_count,))
+
+    # 启动线程
+    print(f"begin scan table {table_name} between {host_source} and {host_target} in {begin_prefix} and {end_prefix}")
+    thread1.start()
+    thread2.start()
+
+    # 等待两个线程执行完毕
+    thread1.join()
+    thread2.join()
+
+    print(
+        f"begin compare table {table_name} between {host_source} and {host_target} in {begin_prefix} and {end_prefix}")
+    compare(task_fix, host_source, host_target, table_name, cache1, cache2, begin_prefix, end_prefix)
+    print(f"end compare table {table_name} between {host_source} and {host_target} in {begin_prefix} and {end_prefix}")
+
+    del cache1
+    del cache2
 
 
 def main():
